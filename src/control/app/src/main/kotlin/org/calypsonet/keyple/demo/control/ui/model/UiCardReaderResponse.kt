00e9f06e641dea81cfcbb98a634423c4f0e2849f
VincentDucrot <vincent.ducrot@cis-france.net>
2026-03-25 09:53:57 +0100

    Add model UiCardReaderResponse

---
+++ src/control/app/src/main/kotlin/org/calypsonet/keyple/demo/control/ui/model/UiCardReaderResponse.kt
@@ -0,0 +1,16 @@
+package org.calypsonet.keyple.demo.control.ui.model
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+import org.calypsonet.keyple.demo.control.domain.model.Status
+
+@Parcelize
+data class UiCardReaderResponse(
+    val status: Status,
+    val authenticationMode: AuthenticationMode,
+    val lastValidationsList: ArrayList<UiValidation>? = null,
+    val titlesList: ArrayList<UiContract>,
+    val errorTitle: String? = null,
+    val errorMessage: String? = null
+) : Parcelable
+
