2ee7cea2310688098c65d028f7f69a3ff4c17e9d
VincentDucrot <vincent.ducrot@cis-france.net>
2026-04-02 10:17:37 +0200

    Use common locations instead of local json file

--- src/control/app/src/main/kotlin/org/calypsonet/keyple/demo/control/data/LocationRepository.kt
+++ src/control/app/src/main/kotlin/org/calypsonet/keyple/demo/control/data/LocationRepository.kt
@@ -13,52 +13,11 @@
 package org.calypsonet.keyple.demo.control.data
 
 import android.content.Context
-import com.google.gson.Gson
-import com.google.gson.GsonBuilder
-import java.io.BufferedReader
-import java.io.IOException
-import java.io.InputStream
-import java.io.InputStreamReader
+import org.calypsonet.keyple.demo.common.data.LocationRepository
+import org.calypsonet.keyple.demo.common.model.Location
 import javax.inject.Inject
-import org.calypsonet.keyple.demo.control.domain.model.Location
 
 class LocationRepository @Inject constructor(context: Context) {
 
-  val locations: List<Location>
-
-  init {
-    locations =
-        getGson()
-            .fromJson(getFileFromResources(context = context), Array<Location>::class.java)
-            .toList()
-  }
-
-  /** Get file from raw embedded directory */
-  private fun getFileFromResources(context: Context): String {
-    val resId = context.resources.getIdentifier("locations", "raw", context.packageName)
-    val inputStream = context.resources.openRawResource(resId)
-    return parseFile(inputStream)
-  }
-
-  private fun parseFile(inputStream: InputStream): String {
-    val sb = StringBuilder()
-    var strLine: String?
-    try {
-      BufferedReader(InputStreamReader(inputStream, "UTF-8")).use { reader ->
-        while (reader.readLine().also { strLine = it } != null) {
-          sb.append(strLine)
-        }
-      }
-    } catch (ignore: IOException) { // ignore
-    }
-    return sb.toString()
-  }
-
-  private fun getGson(): Gson {
-    val gsonBuilder = GsonBuilder()
-    gsonBuilder.disableHtmlEscaping()
-    gsonBuilder.setPrettyPrinting()
-    gsonBuilder.setLenient()
-    return gsonBuilder.create()
-  }
+  val locations: List<Location> = LocationRepository.getLocations()
 }
