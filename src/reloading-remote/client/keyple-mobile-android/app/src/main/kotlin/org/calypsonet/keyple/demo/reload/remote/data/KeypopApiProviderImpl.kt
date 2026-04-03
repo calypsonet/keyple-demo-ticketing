d90dfc9be792a7d31497958d47c6a1882d90d429
VincentDucrot <vincent.ducrot@cis-france.net>
2026-04-03 17:17:38 +0200

    Add KeypopApiProviderImpl

---
+++ src/reloading-remote/client/keyple-mobile-android/app/src/main/kotlin/org/calypsonet/keyple/demo/reload/remote/data/KeypopApiProviderImpl.kt
@@ -0,0 +1,30 @@
+package org.calypsonet.keyple.demo.reload.remote.data
+
+import org.calypsonet.keyple.card.storagecard.StorageCardExtensionService
+import org.calypsonet.keyple.demo.reload.remote.domain.spi.KeypopApiProvider
+import org.eclipse.keyple.card.calypso.CalypsoExtensionService
+import org.eclipse.keyple.card.calypso.crypto.legacysam.LegacySamExtensionService
+import org.eclipse.keyple.core.service.SmartCardServiceProvider
+import org.eclipse.keypop.calypso.card.CalypsoCardApiFactory
+import org.eclipse.keypop.calypso.crypto.legacysam.LegacySamApiFactory
+import org.eclipse.keypop.reader.ReaderApiFactory
+import org.eclipse.keypop.storagecard.StorageCardApiFactory
+
+class KeypopApiProviderImpl : KeypopApiProvider {
+
+    override fun getReaderApiFactory(): ReaderApiFactory {
+        return SmartCardServiceProvider.getService().readerApiFactory
+    }
+
+    override fun getCalypsoCardApiFactory(): CalypsoCardApiFactory {
+        return CalypsoExtensionService.getInstance().calypsoCardApiFactory
+    }
+
+    override fun getLegacySamApiFactory(): LegacySamApiFactory {
+        return LegacySamExtensionService.getInstance().legacySamApiFactory
+    }
+
+    override fun getStorageCardApiFactory(): StorageCardApiFactory {
+        return StorageCardExtensionService.getInstance().storageCardApiFactory
+    }
+}
